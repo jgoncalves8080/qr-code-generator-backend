2dc5ad1bb896ab595aaf6b0c3d9bf3a666355d6c
unknown <goncalves2541@gmail.com>
2021-05-17 23:19:16 +0100

    Add cors

--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ const app = express();
 const port = 3030;
 const bp = require('body-parser');
 const qr = require('qrcode');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.post('/generate', (req, res) => {
-  const { name } = req.body;
-  if (!name) return res.json({ message: 'Empty Data' });
-  qr.toDataURL(name, (err, src) => {
+  const { name, funcao, nif } = req.body;
+  const info = `Nome: ${name} \n Função: ${funcao} \n NIF: ${nif}`;
+
+  if (!info) return res.json({ message: 'Empty Data' });
+  qr.toDataURL(info, (err, src) => {
     if (err) return res.status(400).json({ message: 'Error occured' });
     res.status(200).json({
-      URL: src,
+      data: src,
     });
   });
 });
