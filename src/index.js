a668e5d674c6384a592e5adc7690290e16d36323
unknown <goncalves2541@gmail.com>
2021-05-20 20:46:17 +0100

    change port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = Process.env.PORT || 3030;
+const port = process.env.PORT || 3030;
 const qr = require('qrcode');
 const cors = require('cors');
 
